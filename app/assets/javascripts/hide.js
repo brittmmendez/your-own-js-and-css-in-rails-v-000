f44bd67dda88412b951d8ec6380006e4b34a254e
Brittany Morris <brittmmendez@gmail.com>
2018-03-31 04:22:22 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    $("#hide_this").click(function(){
-        $("a").hide();
-    });
-  });
+function hideWhenClicked(event) {
+  var $target = $(event.target);
+  $target.hide();
+}
+
+$('#hide_this').click(hideWhenClicked);
